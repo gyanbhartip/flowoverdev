dfc703ea57b4208af2c294a92c410720b173fbf7
Gyan Bharti <gyanbhartip@gmail.com>
2024-08-11 14:50:28 +0530

    chore: init (with tw, eslint & prettier setup done).

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <main className="primary-gradient flex min-h-screen flex-col items-center justify-center bg-gradient-to-b text-white">
+      <p>Hello Dev</p>
+    </main>
+  );
+}
